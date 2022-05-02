d577c7bedee82847e1552ee1e06b60f791170472
B3nhard <chumba101benard@gmail.com>
2022-05-02 15:35:46 +0300

    Created Contact Model

--- apps/home/models.py
+++ apps/home/models.py
@@ -3,8 +3,19 @@
 Copyright (c) 2019 - present AppSeed.us
 """
 
+from time import time
 from django.db import models
 from django.contrib.auth.models import User
+from django.utils import timezone
 
 # Create your models here.
+
+class Contact(models.Model):
+    name = models.CharField(max_length=50,null=False)
+    email = models.EmailField( max_length=254,null=False)
+    message = models.TextField(null=False)
+    createdAt = models.DateTimeField( default=timezone.now)
+
+    def __str__(self):
+        return f"Message from {self.name}"
 
